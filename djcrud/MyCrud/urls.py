d030a4219b148f230b67eee9bede09c5beeb12c8
DevAman11 <aman11newus@gmail.com>
2025-01-17 12:53:36 +0530

    save

--- djcrud/MyCrud/urls.py
+++ djcrud/MyCrud/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    path('Homepage',views.Homepage),
+    path('Register/',views.Register),
+    path('Login/',views.Register),
+    path('Userdata/',views.Register)
 ]
 
--- djcrud/MyCrud/views.py
+++ djcrud/MyCrud/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render,HttpResponse
 
 def index(req):
     return render(req,'index.html')
+
+def Homepage(req):
+    return render(req,'Homepage.html')
+
+def Register(req):
+    return render(req,'Register.html')
+
+def Login(req):
+    return render(req,'Login.html')
+
+def Userdata(req):
+    return render(req,'Userdata.html')
