271b950ff38075e2df768372b66186656650d7cb
DevAman11 <aman11newus@gmail.com>
2025-01-21 10:49:21 +0530

    kk

--- djcrud/MyCrud/views.py
+++ djcrud/MyCrud/views.py
@@ -1,35 +1,49 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 from .models import MyTable
 
 # Create your views here.
 
+
 def index(req):
-    return render(req,'index.html')
+    return render(req, 'index.html')
+
 
 def Homepage(req):
-    return render(req,'Homepage.html')
+    return render(req, 'Homepage.html')
+
 
 def Register(req):
-    return render(req,'Register.html')
+    return render(req, 'Register.html')
+
 
 def Login(req):
-    Email=req.POST.get("Email")
-    Password=req.POST.get("Password")
-    LoginData=MyTable.objects.get(Email=Email)
-    if LoginData.Password == Password:
-        return HttpResponse("Login SuccessFully")
+    if req.method == "POST":
+        Email = req.POST.get("Email")
+        Password = req.POST.get("Password")
+        try:
+            # Retrieve the user data based on the email
+            LoginData = MyTable.objects.get(Email=Email)
+            # Check if the provided password matches the stored password
+            if check_password(Password, LoginData.Password):
+                return HttpResponse("Login Successfully")
+            else:
+                return HttpResponse("Password Doesn't Match")
+        except MyTable.DoesNotExist:
+            return HttpResponse("User with this email doesn't exist")
     else:
-        return HttpResponse("Info Doesn't Match")
+        # If GET request, just render the login form
+        return render(req, 'Login.html')  # Adjust 'Login.html' to your actual template
        
 def Userdata(req):
     MyData = MyTable.objects.all()
     objData = {
-        "Rows" : MyData
+        "Rows": MyData
     }
     
-    return render(req,'Userdata.html',context=objData)
+    return render(req, 'Userdata.html', context=objData)
+
 
 def save(req): 
     Firstname = req.POST.get("Fname")
@@ -37,30 +51,31 @@ def save(req):
     Email = req.POST.get("Email")
     Password = req.POST.get("Password")
     
-    MyObj = MyTable(Firstname=Firstname,Lastname=Lastname,Email = Email, Password = Password)
+    MyObj = MyTable(Firstname=Firstname, Lastname=Lastname, Email=Email, Password=Password)
     MyObj.save()
     return redirect('Userdata')
 
 
-
-def Delete(req,id):
-    deleteData=MyTable.objects.get(id=id)
+def Delete(req, id):
+    deleteData = MyTable.objects.get(id=id)
     deleteData.delete()
     return redirect('Userdata')
 
-def Edit(req,id):
-    editdata=MyTable.objects.get(id=id)
+
+def Edit(req, id):
+    editdata = MyTable.objects.get(id=id)
     Data = {
         "editdata": editdata
     }
     
-    return render(req,"Update.html" , context=Data)
+    return render(req, "Update.html" , context=Data)
 
-def Update(req,id):
-    editdata=MyTable.objects.get(id=id)
-    editdata.Firstname=req.POST.get("Fname")
-    editdata.Lastname=req.POST.get("Lname")
-    editdata.Email=req.POST.get("Email")
+
+def Update(req, id):
+    editdata = MyTable.objects.get(id=id)
+    editdata.Firstname = req.POST.get("Fname")
+    editdata.Lastname = req.POST.get("Lname")
+    editdata.Email = req.POST.get("Email")
     editdata.save()
     
     return redirect("Userdata")
